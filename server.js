af7bfab4913c18e002a045ef56b1920a797ae0b0
alvarorojas1101 <alvarorojas1101@gmail.com>
2025-03-14 11:55:26 -0300

    Modificando server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const compression = require("compression");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // Middlewares
 app.use(compression());
